0161bb0ae23e12a321310c301a4e866d70512936
ircamitoc <iraincamitoc@gmail.com>
2023-08-31 01:59:47 +0800

    Added boostrap and added new code

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./register.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <div className="container">
+        <h2 className="text-center mt-1">Register Your Details</h2>
+      </div>
+    </>
+  );
 };
 
 export default Register;

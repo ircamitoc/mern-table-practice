5ed92fbe271954a326d6d1d160abb20523cc69ca
ircamitoc <iraincamitoc@gmail.com>
2023-08-31 22:48:45 +0800

    Added code and files

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,9 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import "./register.css";
-import { Row, Form } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 import Select from "react-select";
+import Spiner from "../../components/Spiner/Spiner";
+import { registerfunc } from "../../services/Apis";
+import { ToastContainer, toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import "./register.css";
+import { addData } from "../../components/context/ContextProvider";
 
 const Register = () => {
   const [inputdata, setInputData] = useState({
@@ -15,19 +22,22 @@ const Register = () => {
     location: "",
   });
 
-  console.log(inputdata);
-
   const [status, setStatus] = useState("Active");
   const [image, setImage] = useState("");
   const [preview, setPreview] = useState("");
+  const [showspin, setShowSpin] = useState(true);
 
-  // status options
+  const navigate = useNavigate();
+
+  const { useradd, setUseradd } = useContext(addData);
+
+  // status optios
   const options = [
     { value: "Active", label: "Active" },
-    { value: "Inactive", label: "Inactive" },
+    { value: "InActive", label: "InActive" },
   ];
 
-  // setInput value
+  // setInput Value
   const setInputValue = (e) => {
     const { name, value } = e.target;
     setInputData({ ...inputdata, [name]: value });
@@ -43,100 +53,208 @@ const Register = () => {
     setImage(e.target.files[0]);
   };
 
+  //submit userdata
+  const submitUserData = async (e) => {
+    e.preventDefault();
+
+    const { fname, lname, email, mobile, sex, location } = inputdata;
+
+    if (fname === "") {
+      toast.error("First name is Required!");
+    } else if (lname === "") {
+      toast.error("Last name is Required!");
+    } else if (email === "") {
+      toast.error("Email is Required!");
+    } else if (!email.includes("@")) {
+      toast.error("Enter Valid Email!");
+    } else if (mobile === "") {
+      toast.error("Mobile is Required!");
+    } else if (mobile.length > 11) {
+      toast.error("Enter Valid Mobile!");
+    } else if (sex === "") {
+      toast.error("Sex is Required!");
+    } else if (status === "") {
+      toast.error("Status is Required!");
+    } else if (image === "") {
+      toast.error("Profile is Required!");
+    } else if (location === "") {
+      toast.error("Location is Required!");
+    } else {
+      console.log(image);
+
+      const data = new FormData();
+      data.append("fname", fname);
+      data.append("lname", lname);
+      data.append("email", email);
+      data.append("mobile", mobile);
+      data.append("sex", sex);
+      data.append("status", status);
+      data.append("user_profile", image);
+      data.append("location", location);
+
+      const config = {
+        "Content-Type": "multipart/form-data",
+      };
+
+      const response = await registerfunc(data, config);
+
+      if (response.status === 200) {
+        setInputData({
+          ...inputdata,
+          fname: "",
+          lname: "",
+          email: "",
+          mobile: "",
+          sex: "",
+          location: "",
+        });
+        setStatus("");
+        setImage("");
+        setUseradd(response.data);
+        navigate("/");
+      } else {
+        toast.error("Error!");
+      }
+    }
+  };
+
   useEffect(() => {
     if (image) {
       setPreview(URL.createObjectURL(image));
     }
+
+    setTimeout(() => {
+      setShowSpin(false);
+    }, 1200);
   }, [image]);
+
   return (
     <>
-      <div className="container">
-        <br />
-        <h2 className="text-center mt-1">Register Your Details</h2>
-        <br />
-        <Card className="shadow mt-3 p-3">
-          <div className="profile_div text-center">
-            <img src={preview ? preview : "/kirby.jpg"} alt="img" />
-          </div>
+      {showspin ? (
+        <Spiner />
+      ) : (
+        <div className="container">
           <br />
-          <Form>
-            <Row>
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={setInputValue}
-                  name="fname"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={setInputValue}
-                  name="lname"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                  type="email"
-                  onChange={setInputValue}
-                  name="email"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Mobile Number</Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={setInputValue}
-                  name="lname"
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Sex</Form.Label>
-                <Form.Check // prettier-ignore
-                  type={"radio"}
-                  label={`Male`}
-                  name="sex"
-                  value={`Male`}
-                  onChange={setInputValue}
-                />
-                <Form.Check // prettier-ignore
-                  type={"radio"}
-                  label={`Female`}
-                  name="sex"
-                  value={`Female`}
-                  onChange={setInputValue}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Status</Form.Label>
-                <Select options={options} onChange={setStatusValue} />
-              </Form.Group>
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Add Profile Picture</Form.Label>
-                <Form.Control
-                  type="file"
-                  name="user_profile"
-                  onChange={setProfile}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-                <Form.Label>Location</Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={setInputValue}
-                  name="location"
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Row>
-          </Form>
-        </Card>
-      </div>
+          <h2 className="text-center mt-1">Register Your Details</h2>
+          <br />
+          <Card className="shadow mt-3 p-3">
+            <div className="profile_div text-center">
+              <img src={preview ? preview : "/kirby.jpg"} alt="img" />
+            </div>
+            <br />
+            <Form>
+              <Row>
+                <Form.Group
+                  className="mb-3 col-lg-6"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>First name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="fname"
+                    value={inputdata.fname}
+                    onChange={setInputValue}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3 col-lg-6"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="lname"
+                    value={inputdata.lname}
+                    onChange={setInputValue}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3 col-lg-6"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    value={inputdata.email}
+                    onChange={setInputValue}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3 col-lg-6"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Mobile</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="mobile"
+                    value={inputdata.mobile}
+                    onChange={setInputValue}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3 col-lg-6"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Sex</Form.Label>
+                  <Form.Check
+                    type={"radio"}
+                    label={`Male`}
+                    name="sex"
+                    value={"Male"}
+                    onChange={setInputValue}
+                  />
+                  <Form.Check
+                    type={"radio"}
+                    label={`Female`}
+                    name="sex"
+                    value={"Female"}
+                    onChange={setInputValue}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3 col-lg-6"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Select Your Status</Form.Label>
+                  <Select options={options} onChange={setStatusValue} />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3 col-lg-6"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Select Your Profile</Form.Label>
+                  <Form.Control
+                    type="file"
+                    name="user_profile"
+                    onChange={setProfile}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3 col-lg-6"
+                  controlId="formBasicEmail"
+                >
+                  <Form.Label>Enter Your Location</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="location"
+                    value={inputdata.location}
+                    onChange={setInputValue}
+                  />
+                </Form.Group>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={submitUserData}
+                >
+                  Submit
+                </Button>
+              </Row>
+            </Form>
+          </Card>
+          <ToastContainer position="top-center" />
+        </div>
+      )}
     </>
   );
 };
